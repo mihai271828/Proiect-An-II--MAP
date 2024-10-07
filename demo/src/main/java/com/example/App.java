eb75f6e241fff949171cdbaaaf079db3e186154f
mihai31415 <developer@fasmr.ro>
2024-10-07 23:38:22 +0300

    age

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -5,5 +5,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Person mihai=new Person(21, null);
+        System.out.println(mihai.doubleAge());
+    }
+}
+class Person{
+    int age;
+    String name;
+    Person(int age, String name){
+        this.age=age;
+        this.name=name;
+    }
+    int doubleAge(){
+        return 2*age;
     }
 }
