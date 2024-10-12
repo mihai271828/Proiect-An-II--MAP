655be3aee891fa59827d274f29eca86d1680a490
mihai31415 <developer@fasmr.ro>
2024-10-12 17:19:52 +0300

    Aufagabe 3 incomplet

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -5,20 +5,23 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
         int[] noten = { 29, 37, 38, 41, 84, 67 };
-        int[] numbers={4, 8, 3, 10, 17};
+        int[] numbers = { 4, 8, 3, 10, 17 };
+        int[] arr1 = { 130000000 };
+        int[] arr2 = { 870000000 };
         Note genug = new Note();
-        Zahlen number=new Zahlen();
+        Zahlen number = new Zahlen();
+        GrosseZahlen zahl = new GrosseZahlen();
         System.out.println("Aufgabe 1 ");
-        System.out.println("AusreichendeNoten: "+Arrays.toString(genug.ausreichendeNoten(noten)));
-        System.out.println("Mean: "+genug.Durchschnittswert(noten));
-        System.out.println("RoundedArray: "+Arrays.toString(genug.RoundedNumber(noten)));
-        System.out.println("RoundedMaxNumber: " +genug.MaxRoundedNumber(noten));
+        System.out.println("AusreichendeNoten: " + Arrays.toString(genug.ausreichendeNoten(noten)));
+        System.out.println("Mean: " + genug.Durchschnittswert(noten));
+        System.out.println("RoundedArray: " + Arrays.toString(genug.RoundedNumber(noten)));
+        System.out.println("RoundedMaxNumber: " + genug.MaxRoundedNumber(noten));
         System.out.println("Aufgabe 2 ");
-        System.out.println("MaximaleSumme: "+number.maximaleSumme(numbers));
-        System.out.println("MinimaleSumme: "+number.minimaleSumme(numbers));
-        System.out.println("MaximaleZahl: "+number.maximaleZahl(numbers));
-        System.out.println("MinimaleZahl: "+number.minimaleZahl(numbers));
-
+        System.out.println("MaximaleSumme: " + number.maximaleSumme(numbers));
+        System.out.println("MinimaleSumme: " + number.minimaleSumme(numbers));
+        System.out.println("MaximaleZahl: " + number.maximaleZahl(numbers));
+        System.out.println("MinimaleZahl: " + number.minimaleZahl(numbers));
+        System.out.println(Arrays.toString(zahl.summe(arr1, arr2)));
 
     }
 
@@ -63,12 +66,13 @@ class Note {
         }
         return noten;
     }
-    int MaxRoundedNumber(int[] noten){
-        int max=0;
-        noten=RoundedNumber(noten);
-        for(int note:noten){
-            if(note>max){
-                max=note;
+
+    int MaxRoundedNumber(int[] noten) {
+        int max = 0;
+        noten = RoundedNumber(noten);
+        for (int note : noten) {
+            if (note > max) {
+                max = note;
             }
         }
         return max;
@@ -76,44 +80,94 @@ class Note {
 
 }
 
-class Zahlen{
-    
-    int maximaleZahl(int[] zahlen){
-        int max=0;
-        for(int i=0;i<zahlen.length;i++){
-            if(zahlen[i]>max){
-                max=zahlen[i];
+class Zahlen {
+
+    int maximaleZahl(int[] zahlen) {
+        int max = 0;
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahlen[i] > max) {
+                max = zahlen[i];
             }
         }
         return max;
     }
-    int minimaleZahl(int[] zahlen){
-        int min=999999;
-        for(int i=0;i<zahlen.length;i++){
-            if(zahlen[i]<min){
-                min=zahlen[i];
+
+    int minimaleZahl(int[] zahlen) {
+        int min = 999999;
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahlen[i] < min) {
+                min = zahlen[i];
             }
         }
         return min;
     }
 
-    int minimaleSumme(int[] zahlen){
-        int sum=0;
-        for(int i=0;i<zahlen.length;i++){
-            if(zahlen[i]<maximaleZahl(zahlen)){
-                sum+=zahlen[i];
+    int minimaleSumme(int[] zahlen) {
+        int sum = 0;
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahlen[i] < maximaleZahl(zahlen)) {
+                sum += zahlen[i];
             }
         }
         return sum;
     }
-    
-    int maximaleSumme(int[] zahlen){
-        int sum=0;
-        for(int i=0;i<zahlen.length;i++){
-           if(zahlen[i]>minimaleZahl(zahlen)){
-            sum+=zahlen[i];
-           }
+
+    int maximaleSumme(int[] zahlen) {
+        int sum = 0;
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahlen[i] > minimaleZahl(zahlen)) {
+                sum += zahlen[i];
+            }
         }
         return sum;
+    }
+}
+
+class IntWrapper {
+    int value;
+}
+
+class GrosseZahlen {
+    int[] summe(int[] arr1, int[] arr2) {
+
+        IntWrapper resultSize = new IntWrapper();
+        int sum = 0;
+        int carry = 0;
+        resultSize.value = 0;
+        int[] result = new int[Math.max(arr1.length, arr2.length) + 1];
+        int i = arr1.length - 1;
+        int j = arr2.length - 1;
+        int digitA, digitB;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            if (i >= 0) {
+                digitA = arr1[i];
+            } else {
+                digitA = 0;
+            }
+            if (j >= 0) {
+                digitB = arr2[j];
+            } else {
+                digitB = 0;
+            }
+            sum = digitA + digitB + carry;
+
+            result[resultSize.value++] = sum % 10;
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+        int[] finalResult = new int[resultSize.value];
+        for (int k = 0; k < resultSize.value; k++) {
+            finalResult[k] = result[k]; // Copy the digits
+        }
+
+        // Reverse the final result to get the correct order
+        for (int k = 0; k < finalResult.length / 2; k++) {
+            int temp = finalResult[k];
+            finalResult[k] = finalResult[finalResult.length - 1 - k];
+            finalResult[finalResult.length - 1 - k] = temp;
+        }
+
+        return finalResult; //
     }
 }
