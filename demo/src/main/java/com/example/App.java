4d40e5797daa25daed7bbf79729b4e24bc9e5a39
mihai31415 <developer@fasmr.ro>
2024-10-13 16:31:59 +0300

    Aufgabe 3.1

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -6,13 +6,14 @@ public class App {
     public static void main(String[] args) {
         int[] noten = { 29, 37, 38, 41, 84, 67 };
         int[] numbers = { 4, 8, 3, 10, 17 };
-        int[] arr1 = { 130000000 };
-        int[] arr2 = { 870000000 };
+        int[] arr1 = { 1,3,0,0,0,0,0,0,0 };
+        int[] arr2 = { 8,7,0,0,0,0,0,0,0 };
+        int[] result={};
         int[] keyboards={40,60};
         int[] usbs={8,12};
         Note genug = new Note();
         Zahlen number = new Zahlen();
-        // GrosseZahlen zahl = new GrosseZahlen();
+        GrosseZahlen zahl = new GrosseZahlen();
         Electronics gadget=new Electronics();
         System.out.println("Aufgabe 1 ");
         System.out.println("AusreichendeNoten: " + Arrays.toString(genug.ausreichendeNoten(noten)));
@@ -24,11 +25,15 @@ public class App {
         System.out.println("MinimaleSumme: " + number.minimaleSumme(numbers));
         System.out.println("MaximaleZahl: " + number.maximaleZahl(numbers));
         System.out.println("MinimaleZahl: " + number.minimaleZahl(numbers));
-        // System.out.println(Arrays.toString(zahl.summe(arr1, arr2)));
         System.out.println(gadget.biligste(keyboards));
         System.out.println(gadget.teursten(keyboards, usbs));
         System.out.println(gadget.affordable(usbs, 30));
         System.out.println(gadget.bothaffordable(60,keyboards,usbs));
+        System.out.println(Arrays.toString(zahl.summe(arr1, arr2,result)));
+
+
+        
+
     }
 
 };
@@ -129,54 +134,47 @@ class Zahlen {
     }
 }
 
-class IntWrapper {
-    int value;
+
+
+class GrosseZahlen {
+    int[] summe(int[] arr1, int[] arr2, int[] result) {
+
+       
+        int maxLength=Math.max(arr1.length, arr2.length) + 1;
+        result = new int[maxLength];
+
+
+        int sum = 0;
+        int carry = 0;
+
+        int i = arr1.length - 1;
+        int j = arr2.length - 1;
+        int k=maxLength-1;
+
+        int digitA, digitB;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            if (i >= 0) {
+                digitA = arr1[i];
+            } else {
+                digitA = 0;
+            }
+            if (j >= 0) {
+                digitB = arr2[j];
+            } else {
+                digitB = 0;
+            }
+            sum = digitA + digitB + carry;
+            result[k] = sum % 10;
+            carry = sum / 10;
+            i--;
+            j--;
+            k--;
+            
+        }
+        return result;
+    }
+    
 }
-
-// class GrosseZahlen {
-//     int[] summe(int[] arr1, int[] arr2) {
-
-//         IntWrapper resultSize = new IntWrapper();
-//         int sum = 0;
-//         int carry = 0;
-//         resultSize.value = 0;
-//         int[] result = new int[Math.max(arr1.length, arr2.length) + 1];
-//         int i = arr1.length - 1;
-//         int j = arr2.length - 1;
-//         int digitA, digitB;
-//         while (i >= 0 || j >= 0 || carry > 0) {
-//             if (i >= 0) {
-//                 digitA = arr1[i];
-//             } else {
-//                 digitA = 0;
-//             }
-//             if (j >= 0) {
-//                 digitB = arr2[j];
-//             } else {
-//                 digitB = 0;
-//             }
-//             sum = digitA + digitB + carry;
-
-//             result[resultSize.value++] = sum % 10;
-//             carry = sum / 10;
-//             i--;
-//             j--;
-//         }
-//         int[] finalResult = new int[resultSize.value];
-//         for (int k = 0; k < resultSize.value; k++) {
-//             finalResult[k] = result[k]; // Copy the digits
-//         }
-
-//         // Reverse the final result to get the correct order
-//         for (int k = 0; k < finalResult.length / 2; k++) {
-//             int temp = finalResult[k];
-//             finalResult[k] = finalResult[finalResult.length - 1 - k];
-//             finalResult[finalResult.length - 1 - k] = temp;
-//         }
-
-//         return finalResult; //
-//     }
-// }
     class Electronics{
         int biligste(int[] keyboard){
             int min=99999;
@@ -225,3 +223,8 @@ class IntWrapper {
 
     }
 
+
+    
+      
+    
+    
